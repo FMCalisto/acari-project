a4130423f7027f7d8b90feb9f6bb9b6512033f00
Francisco Maria Calisto <email@franciscocalisto.me>
2017-04-20 15:31:26 +0100

    [CREATE] Background Image

--- src/main.m
+++ src/main.m
@@ -15,42 +15,61 @@ imgbk = imread('../frames/SonofMated10/SonofMated1000262.jpg');
 thr = 25;
 minArea = 50;
 baseNum = 262;
-seqLength = 100;
+seqLength = 5333;
 
 se = strel('disk',3);
 
-%figure;
-for i=0:seqLength
-    %imgfr = imread(sprintf('../frames/SonofMated2/SonofMated2%.5d.jpg',baseNum+i));
-    imgfr = imread(sprintf('../frames/SonofMated10/SonofMated10%.5d.jpg',baseNum+i));
-    hold off
-    imshow(imgfr);
-    
-    imgdif = (abs(double(imgbk(:,:,1))-double(imgfr(:,:,1)))>thr) | ...
-        (abs(double(imgbk(:,:,2))-double(imgfr(:,:,2)))>thr) | ...
-        (abs(double(imgbk(:,:,3))-double(imgfr(:,:,3)))>thr);
-    
-    bw = imclose(imgdif,se);
-    %imshow(bw)
-    [lb num]=bwlabel(bw);
-    regionProps = regionprops(lb,'area','FilledImage','Centroid');
-    inds = find([regionProps.Area]>minArea);
-    
-    regnum = length(inds);
-    
-    if regnum
-        for j=1:regnum
-            [lin col]= find(lb == inds(j));
-            upLPoint = min([lin col]);
-            dWindow  = max([lin col]) - upLPoint + 1;
-           
-            rectangle('Position', ...
-                      [fliplr(upLPoint) fliplr(dWindow)], ...
-                      'EdgeColor',[1 1 0], ...
-                      'linewidth',2);
-        end
-    end
-    drawnow
+nFrame= 40*25;
+step=20;
+
+Bkg=zeros(size(imgbk));
+alfa=0.05;
+figure; hold on
+
+%Exprimentar varios valores para ALPHA
+
+for i = 1 : step : nFrame
+    i
+    imgBase = imread(sprintf('../frames/SonofMated10/SonofMated10%.5d.jpg',baseNum+i));
+    Y = imgBase;
+    Bkg = alfa * double(Y) + (1-alfa) * double(Bkg);
+    imshow(uint8(Bkg)); drawnow
 end
+
+% ------------ Fim ------------------- %
+
+%figure;
+% for i=0:seqLength
+%     %imgfr = imread(sprintf('../frames/SonofMated2/SonofMated2%.5d.jpg',baseNum+i));
+%     imgfr = imread(sprintf('../frames/SonofMated10/SonofMated10%.5d.jpg',baseNum+i));
+%     hold off
+%     %imshow(imgfr);
+%     
+%     imgdif = (abs(double(imgbk(:,:,1))-double(imgfr(:,:,1)))>thr) | ...
+%         (abs(double(imgbk(:,:,2))-double(imgfr(:,:,2)))>thr) | ...
+%         (abs(double(imgbk(:,:,3))-double(imgfr(:,:,3)))>thr);
+%     
+%     bw = imclose(imgdif,se);
+%     %imshow(bw)
+%     [lb num]=bwlabel(bw);
+%     regionProps = regionprops(lb,'area','FilledImage','Centroid');
+%     inds = find([regionProps.Area]>minArea);
+%     
+%     regnum = length(inds);
+%     
+%     if regnum
+%         for j=1:regnum
+%             [lin col]= find(lb == inds(j));
+%             upLPoint = min([lin col]);
+%             dWindow  = max([lin col]) - upLPoint + 1;
+%            
+%             rectangle('Position', ...
+%                       [fliplr(upLPoint) fliplr(dWindow)], ...
+%                       'EdgeColor',[1 1 0], ...
+%                       'linewidth',2);
+%         end
+%     end
+%     %drawnow
+% end
     
     
